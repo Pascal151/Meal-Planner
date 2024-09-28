614bbd4c7a7512fad5b6bccf87728da212776e45
Pascal Spors <pascal.spors@gmx.de>
2024-09-28 13:26:56 +0200

    fix unwanted None value prints

--- output/meal_plan_presenter.py
+++ output/meal_plan_presenter.py
@@ -1,5 +1,8 @@
 def present_meal_plan(meal_plan_data_list):
     print(f"Your {meal_plan_data_list[0]} meals will be:")
-    print([print(f"{n + 1} : {item}") for n, item in enumerate(meal_plan_data_list[2])])
-    print("This is what you have to buy in order to prepare:")
-    print([print(item) for item in meal_plan_data_list[3]])
+    for n, item in enumerate(meal_plan_data_list[2]):
+        print(f"{n + 1} : {item}")
+    
+    print("\nThis is what you have to buy in order to prepare:")
+    for item in meal_plan_data_list[3]:
+        print(item)
